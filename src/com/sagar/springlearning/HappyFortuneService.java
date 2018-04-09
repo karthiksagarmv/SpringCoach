5ca5954e978e32df0a95ff820514f286823e9223
Karthik Sagar M V <karthiksagarmv@gmail.com>
2018-04-09 09:56:02 +0530

    Update HappyFortuneService
    Refactoring the code to return random fortune in right way

--- src/com/sagar/springlearning/HappyFortuneService.java
+++ src/com/sagar/springlearning/HappyFortuneService.java
@@ -4,12 +4,19 @@ import java.util.Random;
 
 public class HappyFortuneService implements FortuneService {
 	
+	private String [] fortunes = {
+		"You will meet a lucky girl today",
+		"You will be calm and peaceful today",
+		"you need to work hard as well as smart"
+	}
+	
+	private Random random = new Random();
+	
 	@Override
 	public String getFortune() {
 		// TODO Auto-generated method stub
-		String [] fortunes = {"lucky","enjoy","ossom" };
-		Random random = new Random();
-		return fortunes[random.nextInt(3)];
+		int index = random.nextInt(3);
+		return fortunes[index];
 	}
 
 }
